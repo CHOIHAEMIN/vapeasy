be0f30ca846c42eda49ad15a272b58b96a0df019
CHOIHAEMIN <sjhk02@gmail.com>
2021-12-27 03:24:01 +0900

    프로젝트 최초 커밋

---
+++ vapeasy/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Review
+# Create your views here.
+
+def index(request):
+    review_list = Review.objects.order_by('-create_date')
+    context = {'review_list': review_list}
+    return render(request, 'portfolio_review_list.html', context)
