54d9661c32e5f6088354ddf1a0b61e1eb2cefa3f
CHOIHAEMIN <sjhk02@gmail.com>
2021-12-28 18:37:16 +0900

    1228 모델단 author속성 추가

--- vapeasy/models.py
+++ vapeasy/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Review(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='review_author')
     create_date = models.DateTimeField()
     def __str__(self):
         return self.subject
