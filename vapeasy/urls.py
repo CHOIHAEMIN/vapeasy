3c0e194e5fed04398def6c82d65de33f8ceb4a85
CHOIHAEMIN <sjhk02@gmail.com>
2021-12-28 18:49:21 +0900

    1228 리뷰리스트, 디테일 템플릿과 뷰단 붙이기 완료

--- vapeasy/urls.py
+++ vapeasy/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from .import views
 
+app_name = 'vapeasy'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('review_list/', views.review_list, name='review_list'),
+    path('review_detail/<int:review_id>/', views.review_datail, name='review_detail'),
 ]
--- vapeasy/views.py
+++ vapeasy/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Review
 # Create your views here.
 
 def index(request):
+    return render(request, 'vapeasy/portfolio_main.html')
+
+def review_list(request):
     review_list = Review.objects.order_by('-create_date')
     context = {'review_list': review_list}
     return render(request, 'vapeasy/portfolio_review_list.html', context)
+
+def review_datail(request,review_id):
+    review = get_object_or_404(Review, pk=review_id)
+    context = {'review': review}
+    return render(request, 'vapeasy/portfolio_review_detail.html', context)
