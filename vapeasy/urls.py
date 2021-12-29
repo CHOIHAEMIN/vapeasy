952d0834309fcb1777e1f15f36d6a57623c56c3f
CHOIHAEMIN <sjhk02@gmail.com>
2021-12-29 18:51:52 +0900

    1229 review_create뷰단 생성완료 최해민

--- vapeasy/urls.py
+++ vapeasy/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('survey/', views.survey, name='survey'),
     path('review_list/', views.review_list, name='review_list'),
     path('review_detail/<int:review_id>/', views.review_datail, name='review_detail'),
+    path('review/create/', views.review_create, name='review_create'),
 ]
--- vapeasy/views.py
+++ vapeasy/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.utils import timezone
 from .models import Review
+from .forms import ReviewForm
 # Create your views here.
 
 def index(request):
@@ -14,7 +16,20 @@ def review_list(request):
     context = {'review_list': review_list}
     return render(request, 'vapeasy/review_list.html', context)
 
-def review_datail(request,review_id):
+def review_datail(request, review_id):
     review = get_object_or_404(Review, pk=review_id)
     context = {'review': review}
     return render(request, 'vapeasy/portfolio_review_detail.html', context)
+
+def review_create(request):
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            # review.author = request.user
+            review.create_date = timezone.now()
+            review.save()
+            return redirect('vapeasy:review_list')
+    else:
+        form = ReviewForm()
+    return render(request, 'vapeasy/review_form.html', {'form': form})
