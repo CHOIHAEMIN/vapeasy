556c6b96d1ad5e8735fd02d8b35d62229e2aef2c
CHOIHAEMIN <sjhk02@gmail.com>
2022-01-04 11:40:02 +0900

    0104 파이썬웹개발반 마지막 커밋

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#dpw$s^8jxr^2#tr2=%__sf86odrcpf21*au$lx7_%cbn%--7='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [    '52.78.246.150',
+                     'vapeasy.shop',
+]
 
 
 # Application definition
---
+++ config/settings/prod.py
@@ -0,0 +1,10 @@
+from .base import *
+
+
+ALLOWED_HOSTS = [
+    '52.78.246.150',
+    'vapeasy.shop',
+]
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
+DEBUG = False
