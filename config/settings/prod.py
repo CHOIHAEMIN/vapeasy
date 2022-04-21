ff766cffc79113490f7dc1557b3bb2e892f9bee2
CHOIHAEMIN <sjhk02@gmail.com>
2022-04-21 22:29:58 +0900

    prod설정2

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,8 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['3.39.127.142', '.vapeasy.shop']
+# ALLOWED_HOSTS = ['3.39.127.142', '.vapeasy.shop']
+ALLOWED_HOSTS = ["3.39.127.142", "vapeasy.shop"]
+
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
